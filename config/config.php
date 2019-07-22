0d52b5c1fd69049566439f41a35d32e9f787dd03
luowei <786407418@qq.com>
2019-07-22 20:42:11 +0800

    mod

--- config/config.php
+++ config/config.php
@@ -12,28 +12,12 @@ return [
         "web_socket_port"=>9501,
         "host"=>"0.0.0.0",
     ],
-//    'mysql'=>[
-//        'host' => '114.215.169.104',
-//        'port' => 8066,
-//        'user' => 'hlj_01',
-//        'password' => 'SJ7238#kjwUoooxx',
-//        'database' => 'peiyin',
-//    ]
-
-        'mysql'=>[
-    'host' => '192.168.90.82',
-    'port' => 3306,
-    'user' => 'root',
-    'password' => 'root',
-    'database' => 'chat',
-]
-
-//    $config['mysql']['py']['host'] = '114.215.169.104';
-//$config['mysql']['py']['port'] = '8066';
-//$config['mysql']['py']['user'] = 'hlj_01';
-//$config['mysql']['py']['password'] = 'SJ7238#kjwUoooxx';
-//$config['mysql']['py']['database'] = 'peiyin';
-//$config['mysql']['py']['charset'] = 'utf8';
-
+    'mysql'=>[
+        'host' => '192.168.90.82',
+        'port' => 3306,
+        'user' => 'root',
+        'password' => 'root',
+        'database' => 'chat',
+    ]
 
 ];
